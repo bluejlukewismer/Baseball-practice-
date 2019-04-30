9669ab4c61ffb18be3488f27abc13b76df71b6ca
Luke <luke.wismer@hotmail.com>
2019-04-30 17:16:19 -0400

    Initial sharing of project

---
+++ Baseball.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Baseball here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Baseball
+{
+    // instance variables - replace the example below with your own
+    private String position;
+
+    /**
+     * Constructor for objects of class Baseball
+     */
+    public Baseball(int newPosition)
+    {
+        // initialise instance variables
+        position = newPosition;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
